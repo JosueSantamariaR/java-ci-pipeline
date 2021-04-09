43c4ef79a5f9f8543a3d6a0d775fbfc02e93acf4
Josue Santamaria <gabosr16@gmail.com>
2021-04-09 08:50:38 -0600

    Prueba

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+//HOla
 public class Calculator {
 
       int suma(int a, int b){
--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -1,28 +1,23 @@
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class CalculatorTest {
     Calculator cal =new Calculator();
     @Test
     public void suma() {
-
         assertEquals(10, cal.suma(2,8),0);
         assertEquals(16, cal.suma(10,6),0);
     }
-
     @Test
     public void resta() {
         assertEquals(4, cal.resta(10,6),0);
         assertEquals(7, cal.resta(14,7),0);
     }
-
     @Test
     public void multi() {
         assertEquals(22, cal.multi(11,2),0);
         assertEquals(15, cal.multi(5,3),0);
     }
-
     @Test
     public void div() {
         assertEquals(8,cal.div(64,8),0);
